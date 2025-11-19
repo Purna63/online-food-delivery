c6c4aa78840dcd5e8c7b9a44148c04f16cda4409
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2025-11-19 14:55:30 +0530

    Add files via upload

---
+++ frontend/src/pages/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="policy-page">
+      <h1>Contact Us</h1>
+
+      <p>If you have issues regarding orders, payment, or delivery, contact us:</p>
+
+      <h2>Business Owner</h2>
+      <p>
+        <strong>Name:</strong> Purna Chandra Pradhan <br />
+        <strong>Address:</strong> Rahama, Odisha, India <br />
+      </p>
+
+      <h2>Phone Numbers</h2>
+      <p>
+        9777834155  
+        <br />
+        7750946466
+      </p>
+
+      <h2>Email</h2>
+      <p>purnachandraofficial63@gmail.com</p>
+
+      <h2>Social Links</h2>
+      <p>
+        Instagram, Twitter, LinkedIn links available in footer.
+      </p>
+    </div>
+  );
+};
+
+export default Contact;
