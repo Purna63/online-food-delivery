cd527b9621851d57c0df2a7eb7c3e994196a3623
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2026-03-14 22:07:28 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -42,6 +42,8 @@ const PlaceOrder = () => {
   const [distance, setDistance] = useState(null);
   const [showMap, setShowMap] = useState(false);
   const [isLocationLocked, setIsLocationLocked] = useState(false);
+  const [showAddressForm, setShowAddressForm] = useState(true);//new add
+  const [hasSavedAddress, setHasSavedAddress] = useState(false);//new add
 
   const onChangeHandler = (e) => {
     const { name, value } = e.target;
@@ -223,17 +225,18 @@ const PlaceOrder = () => {
     const phoneRegex = /^\d{10}$/;
     let newErrors = { email: "", phone: "" };
 
-    if (!emailRegex.test(data.email)) {
+    if (showAddressForm && !emailRegex.test(data.email)) {
       newErrors.email = "Invalid email address";
       valid = false;
     }
 
-    if (!phoneRegex.test(data.phone)) {
+    if (showAddressForm && !phoneRegex.test(data.phone)) {
       newErrors.phone = "Phone must be 10 digits";
       valid = false;
     }
 
     setErrors(newErrors);
+    
     if (!valid) {
       setErrorVisible(true);
       setTimeout(() => setErrorVisible(false), 4000);
@@ -257,8 +260,10 @@ const PlaceOrder = () => {
 
       if (!isOpenNow) {
         const nextOpen = new Date(now);
-        if (currentHour >= closeHour || isManuallyClosed)
+        if (currentHour >= closeHour || isManuallyClosed) {
           nextOpen.setDate(nextOpen.getDate() + 1);
+        }
+
         nextOpen.setHours(openHour, 0, 0, 0);
 
         const diff = nextOpen - now;
@@ -308,6 +313,52 @@ const PlaceOrder = () => {
     }
   }, []);
 
+
+   useEffect(() => {
+    const loadAddress = async () => {
+      try {
+        const res = await axios.get(`${BACKEND_URL}/api/user/get-address`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+
+        if (
+          res.data.success &&
+          res.data.street &&
+          res.data.landmark &&
+          res.data.lat &&
+          res.data.lng
+        ) {
+          setHasSavedAddress(true);
+
+          setData((prev) => ({
+            ...prev,
+            street: res.data.street,
+            landmark: res.data.landmark,
+            lat: res.data.lat,
+            lng: res.data.lng,
+            firstName: res.data.name || "",
+            phone: res.data.phone || "",
+          }));
+
+          // calculate delivery fee again
+          const distanceRes = await axios.post(`${BACKEND_URL}/api/distance`, {
+            lat: res.data.lat,
+            lng: res.data.lng,
+          });
+
+          setDistance(distanceRes.data.distance);
+          setDeliveryFee(distanceRes.data.deliveryCharge);
+
+          setShowAddressForm(false);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    if (token) loadAddress();
+  }, [token]);
+
   
 
   const placeOrder = (e) => {
@@ -339,9 +390,7 @@ const PlaceOrder = () => {
     setShowPaymentModal(true);
   };
 
-  const handlePaymentOption = async (option) => {
-
-    //Add New line
+  const handlePaymentOption = async () => {
     if (!distance) {
       setLocationMessage(
         "⚠️ Please select your delivery location first using 'Use My Current Location' or 'Select Location On Map'.",
@@ -354,7 +403,6 @@ const PlaceOrder = () => {
       return;
     }
 
-    
     await fetchStoreStatus();
 
     if (!isStoreOpen) {
@@ -363,73 +411,94 @@ const PlaceOrder = () => {
       return;
     }
 
-    //add new line
     // const totalAmount = getTotalCartAmount() + Deliverycharge;
     const totalAmount = getTotalCartAmount() + deliveryFee;
+
     const orderData = {
       cartItems,
       food_list,
       deliveryInfo: data,
-      payment: true, // online payment only
+      payment: true,
     };
 
     const saveDeliveryInfo = () => {
       localStorage.setItem("deliveryInfo", JSON.stringify(data));
     };
 
-    if (option === "Online Payment") {
-      try {
-        setIsLoadingPayment(true);
-        const script = document.createElement("script");
-        script.src = "https://checkout.razorpay.com/v1/checkout.js";
-        script.async = true;
-        document.body.appendChild(script);
+    try {
+      setIsLoadingPayment(true);
 
-        script.onload = async () => {
-          const response = await fetch(
-            `${BACKEND_URL}/api/payment/create-order`,
-            {
+      const script = document.createElement("script");
+      script.src = "https://checkout.razorpay.com/v1/checkout.js";
+      script.async = true;
+      document.body.appendChild(script);
+
+      script.onload = async () => {
+        const response = await fetch(
+          `${BACKEND_URL}/api/payment/create-order`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ amount: totalAmount * 100 }),
+          },
+        );
+
+        const order = await response.json();
+
+        const options = {
+          key: RAZORPAY_KEY_ID,
+          amount: order.amount,
+          currency: "INR",
+          name: "Food Delivery",
+          order_id: order.id,
+          handler: async function () {
+            await fetch(`${BACKEND_URL}/api/order`, {
               method: "POST",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify({ amount: totalAmount * 100 }),
-            }
-          );
-          const order = await response.json();
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`,
+              },
+              body: JSON.stringify(orderData),
+            });
 
-          const options = {
-            key: RAZORPAY_KEY_ID,
-            amount: order.amount,
-            currency: "INR",
-            name: "Food Delivery",
-            order_id: order.id,
-            handler: async function () {
-              await fetch(`${BACKEND_URL}/api/order`, {
-                method: "POST",
+            saveDeliveryInfo();
+            await axios.post(
+              `${BACKEND_URL}/api/user/save-address`,
+              {
+                street: data.street,
+                landmark: data.landmark,
+                //new add second
+                lat: data.lat,
+                lng: data.lng,
+              },
+              {
                 headers: {
-                  "Content-Type": "application/json",
                   Authorization: `Bearer ${token}`,
                 },
-                body: JSON.stringify(orderData),
-              });
-              saveDeliveryInfo();
-              navigate("/order-success");
-            },
-            prefill: {
-              name: `${data.firstName} ${data.lastName}`,
-              email: data.email,
-              contact: data.phone,
-            },
-            theme: { color: "#F37254" },
-          };
-
-          setIsLoadingPayment(false);
-          const paymentObject = new window.Razorpay(options);
-          paymentObject.open();
+              },
+            );
+            navigate("/order-success");
+          },
+          prefill: {
+            name: `${data.firstName} ${data.lastName}`,
+            email: data.email,
+            contact: data.phone,
+          },
+          theme: {
+            color: "#F37254",
+          },
         };
-      } catch (err) {
-        console.error("Payment error", err);
+
         setIsLoadingPayment(false);
-      }
+
+        const paymentObject = new window.Razorpay(options);
+        paymentObject.open();
+      };
+    } catch (err) {
+      console.error("Payment error", err);
+      setIsLoadingPayment(false);
     }
   };
 
@@ -457,133 +526,140 @@ const PlaceOrder = () => {
       <form onSubmit={placeOrder} className="place-order">
         <div className="place-order-left">
           <p className="title">Delivery Information</p>
-          <div className="multi-fields">
-            <input
-              required
-              name="firstName"
-              onChange={onChangeHandler}
-              value={data.firstName}
-              type="text"
-              placeholder="Enter Your Name"
-            />
-          </div>
-          <input
-            name="email"
-            onChange={onChangeHandler}
-            value={data.email}
-            type="email"
-            placeholder="Email address"
-          />
-          {errors.email && <p className="error-message">{errors.email}</p>}
-          <div className="multi-fields">
-            {/* <input
-              required
-              name="street"
-              onChange={onChangeHandler}
-              value={data.street}
-              type="text"
-              placeholder="Street / Village"
-            /> */}
-            <input
-              required
-              name="street"
-              value={data.street}
-              onChange={onChangeHandler}
-              type="text"
-              placeholder="Street / Village"
-              readOnly={isLocationLocked}
-            />
-            <input
-              required
-              name="landmark"
-              onChange={onChangeHandler}
-              value={data.landmark}
-              type="text"
-              placeholder="Landmark"
-            />
-          </div>
-          <div className="multi-fields">
-            <input
-              required
-              name="city"
-              onChange={onChangeHandler}
-              value={data.city}
-              type="text"
-              placeholder="City"
-            />
-            <input
-             name="state"
-              value="Odisha"
-              readOnly
-            />
-          </div>
-          <div className="multi-fields">
-            <input
-              required
-              name="zipcode"
-              onChange={onChangeHandler}
-              value={data.zipcode}
-              type="text"
-              placeholder="Pin Code/Zip Code"
-            />
-            <input
-              name="country"
-              value="India"
-              readOnly
-            />
-          </div>
-          <input
-            required
-            name="phone"
-            onChange={onChangeHandler}
-            value={data.phone}
-            type="text"
-            placeholder="Mobile No."
-          />
 
+          {!showAddressForm ? (
+            <div className="saved-address-box">
+              <p>
+                <strong>{data.firstName || "Customer"}</strong>
+              </p>
+              <p>{data.street}</p>
+              <p>{data.landmark}</p>
+              <p>{data.phone}</p>
 
-          {/* new line add */}
+              <button
+                className="change-address-btn"
+                type="button"
+                onClick={() => setShowAddressForm(true)}
+              >
+                Change
+              </button>
+            </div>
+          ) : (
+            <>
+              <div className="multi-fields">
+                <input
+                  required
+                  name="firstName"
+                  onChange={onChangeHandler}
+                  value={data.firstName}
+                  type="text"
+                  placeholder="Enter Your name"
+                />
+              </div>
 
-          <p style={{ fontSize: "14px", color: "#555", marginTop: "10px" }}>
-            📍 Please select your delivery location using one of the buttons
-            below.
-          </p>
+              <input
+                name="email"
+                onChange={onChangeHandler}
+                value={data.email}
+                type="email"
+                placeholder="Email address"
+              />
 
-          <button
-            type="button"
-            className="location-btn"
-            onClick={resetLocation}
-          >
-            Reset Address
-          </button>
+              {errors.email && <p className="error-message">{errors.email}</p>}
 
-          <br />
+              <div className="multi-fields">
+                <input
+                  required
+                  name="street"
+                  value={data.street}
+                  onChange={onChangeHandler}
+                  type="text"
+                  placeholder="Street / Village"
+                  readOnly={isLocationLocked}
+                />
 
-          {/* STEP 6 NEW BUTTON */}
-          <div className="location-buttons">
-            <button
-              className="location-btn"
-              type="button"
-              onClick={getLocation}
-            >
-              Use My Current Location
-            </button>
+                <input
+                  required
+                  name="landmark"
+                  onChange={onChangeHandler}
+                  value={data.landmark}
+                  type="text"
+                  placeholder="Landmark"
+                />
+              </div>
 
-            <button
-              type="button"
-              className="location-btn"
-              onClick={() => setShowMap(true)}
-            >
-              Select Location On Map
-            </button>
-          </div>
+              <div className="multi-fields">
+                <input
+                  required
+                  name="city"
+                  onChange={onChangeHandler}
+                  value={data.city}
+                  type="text"
+                  placeholder="City"
+                />
 
-          {showMap && <LocationPicker setLocation={setLocation} />}
+                <input
+                  required
+                  name="zipcode"
+                  onChange={onChangeHandler}
+                  value={data.zipcode}
+                  type="text"
+                  placeholder="Pin Code"
+                />
+              </div>
 
+              <div className="multi-fields">
+                <input name="state" value="Odisha" readOnly />
+                <input name="country" value="India" readOnly />
+              </div>
 
+              <input
+                required
+                name="phone"
+                onChange={onChangeHandler}
+                value={data.phone}
+                type="text"
+                placeholder="Mobile No."
+              />
 
-          
-          {errors.phone && <p className="error-message">{errors.phone}</p>}
+              <p style={{ fontSize: "14px", color: "#555", marginTop: "10px" }}>
+                📍 Please select your delivery location using one of the buttons
+                below.
+              </p>
+
+              <button
+                type="button"
+                className="location-btn"
+                onClick={resetLocation}
+              >
+                Reset Address
+              </button>
+
+              <br />
+
+              <div className="location-buttons">
+                <button
+                  className="location-btn"
+                  type="button"
+                  onClick={getLocation}
+                >
+                  Use My Current Location
+                </button>
+
+                <button
+                  type="button"
+                  className="location-btn"
+                  onClick={() => setShowMap(true)}
+                >
+                  Select Location On Map
+                </button>
+              </div>
+
+              {showMap && <LocationPicker setLocation={setLocation} />}
+
+              {errors.phone && <p className="error-message">{errors.phone}</p>}
+            </>
+          )}
         </div>
 
         <div className="place-order-right">
