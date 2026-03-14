d52e55d53cfae5516289e9abf9285494b7253631
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2026-03-14 22:10:06 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,6 +4,11 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   phone: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+
+  street: { type: String, default: "" },
+  landmark: { type: String, default: "" },
+  lat: { type: Number, default: null },
+  lng: { type: Number, default: null },
 });
 
 const userModel = mongoose.model("User", userSchema);
