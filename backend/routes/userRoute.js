59e7f377638af11a612d7a7e33d02e19d04e5e0e
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2026-03-14 22:25:58 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,14 @@
 
 
 import express from "express";
-import { loginUser, registerUser, resetPassword } from "../controllers/userController.js"; // Added resetPassword
-
+import { loginUser, registerUser, resetPassword, saveAddress, getAddress } from "../controllers/userController.js"; // Added resetPassword
+import authMiddleware from "../middleware/auth.js";
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/reset-password", resetPassword); // Added the route for reset password
+userRouter.post("/save-address", authMiddleware, saveAddress);
+userRouter.get("/get-address", authMiddleware, getAddress);
 
 export default userRouter;
