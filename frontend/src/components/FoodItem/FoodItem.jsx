eb8893a3097f94de041d2472e6d1ed4aa629d465
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2026-04-21 20:02:15 +0530

    Update FoodItem.jsx

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -3,20 +3,28 @@ import "./FoodItem.css";
 import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
 
-const FoodItem = ({ id, name, price, description, image, shopName, isAvailable, shopStatus, }) => {
-  const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
+const FoodItem = ({
+  id,
+  name,
+  price,
+  description,
+  image,
+  shopName,
+  isAvailable,
+  shopStatus,
+}) => {
+  const { cartItems, addToCart, removeFromCart, url } =
+    useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        <img className="food-item-image" src={url + "/images/" + image} alt="" />
-        {!cartItems[id] ? (
-          <img
-            className="add"
-            onClick={() => addToCart(id)}
-            src={assets.add_icon_white}
-            alt=""
-          />
+        <img
+          className="food-item-image"
+          src={url + "/images/" + image}
+          alt=""
+        />
+
         {shopStatus === "closed" ? (
           <button className="closed-btn">Closed</button>
         ) : isAvailable === false ? (
@@ -44,11 +52,13 @@ const FoodItem = ({ id, name, price, description, image, shopName, isAvailable, 
           </div>
         )}
       </div>
+
       <div className="food-item-info">
         <div className="food-item-name-rating">
           <p>{name}</p>
           <img src={assets.rating_starts} alt="" />
         </div>
+
         <p className="food-item-desc">{description}</p>
         <p className="food-item-price">₹{price}</p>
         <p className="shop-name">🏪 {shopName}</p>
