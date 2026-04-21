da9b9205b142bede5c48ddacd0f4c9b857552643
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2026-04-21 18:58:41 +0530

    Update foodModel.js

--- backend/models/foodModel.js
+++ backend/models/foodModel.js
@@ -6,6 +6,12 @@ const foodSchema = new mongoose.Schema({
   price: { type: Number, required: true },
   image: { type: String, required: true },
   category: { type: String, required: true },
+
+    // NEW UPDATE START
+  shopName: { type: String, default: "Rahama Market" },
+  isAvailable: { type: Boolean, default: true }, // food available
+  shopStatus: { type: String, default: "open" }, // open / closed
+  // NEW UPDATE END
 });
 
 const foodModel = mongoose.models.food || mongoose.model("food", foodSchema);
