4085e36e2a7c73bb80537e87ef31e60a6609866f
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2026-05-13 16:43:08 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -95,6 +95,7 @@ import food_79 from "./food_79.png";
 import food_80 from "./food_80.png";
 import food_81 from "./food_81.png";
 import food_82 from "./food_82.png";
+import food_83 from "./food_83.png";
 
 import add_icon_white from "./add_icon_white.png";
 import add_icon_green from "./add_icon_green.png";
@@ -919,6 +920,15 @@ export const food_list = [
     name: "Chicken Biriyani",
     image: food_82,
     price: 120,
+    description:
+      "Food provides essential nutrients for overall health and well-being",
+    category: "Chicken Biriyani",
+  },
+      {
+    _id: "83",
+    name: "Chicken Biriyani",
+    image: food_83,
+    price: 150,
     description:
       "Food provides essential nutrients for overall health and well-being",
     category: "Chicken Biriyani",
