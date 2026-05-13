9ab0c25810d3d61755c7045d7dd5ef5c226df498
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2026-05-13 14:46:51 +0530

    Update List.jsx

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -91,7 +91,7 @@ const List = ({ url }) => {
           {list.map((item, index) => {
             return (
               <div key={index} className="list-table-format">
-                <img src={`${url}/images/` + item.image} alt={item.name} />
+                <img src={item.image} alt={item.name} />
                 <p>{item.name}</p>
                 <p>{item.category}</p>
                 <p>${item.price}</p>
