c8a0ea473ea7ba436b5eb60dd76faf5a9f933b42
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2026-05-23 18:57:30 +0530

    Update orderModel.js

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,21 +1,34 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: String, required: true }, // Store user ID
-  items: { type: Array, required: true }, // Ordered food items
-  amount: { type: Number, required: true }, // Total price
-  address: { type: Object, required: true }, // Delivery address
-  status: { type: String, default: "Food Processing" }, // Order status
-  date: { type: Date, default: Date.now }, // Optional custom order date
-  payment: { type: Boolean, default: false }, // Payment success or not
+  userId: { type: String, required: true },
+
+  items: { type: Array, required: true },
+
+  amount: { type: Number, required: true },
+
+  address: { type: Object, required: true },
+
+  status: { type: String, default: "Food Processing" },
+
+  date: { type: Date, default: Date.now },
+
+  payment: { type: Boolean, default: false },
+
   deliveryCharge: { type: Number },
+
   distance: { type: Number },
-   name: { type: String },
+
+  name: { type: String },
+
   phone: { type: String },
 
-}, { timestamps: true }); // ✅ Adds createdAt and updatedAt fields
+  // IMPORTANT FOR WEBHOOK
+  razorpayOrderId: { type: String },
+
+}, { timestamps: true });
 
 const orderModel =
   mongoose.models.order || mongoose.model("order", orderSchema);
-export default orderModel;
 
+export default orderModel;
