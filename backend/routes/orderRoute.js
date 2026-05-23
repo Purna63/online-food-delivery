e26caab7fccaf5f2062bf8b4f23332ad7ed0236a
Mr.Hacker@ <78805865+Purna63@users.noreply.github.com>
2026-05-23 19:29:40 +0530

    Update orderRoute.js

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -6,12 +6,9 @@ import userModel from "../models/userModel.js";
 
 const router = express.Router();
 
-
-
 // PLACE ORDER
 router.post("/", authMiddleware, async (req, res) => {
   try {
-
     console.log(req.body);
 
     const userId = req.userId;
@@ -26,18 +23,12 @@ router.post("/", authMiddleware, async (req, res) => {
       razorpayOrderId,
     } = req.body;
 
-
-
     // RESTAURANT LOCATION
     const RESTAURANT_LAT = 20.302559;
     const RESTAURANT_LNG = 86.404263;
 
-
-
     const userLat = parseFloat(deliveryInfo.lat);
     const userLng = parseFloat(deliveryInfo.lng);
-
-
 
     // IMPORTANT FIX
     if (
@@ -52,17 +43,13 @@ router.post("/", authMiddleware, async (req, res) => {
       });
     }
 
-
-
     // DISTANCE
     const distance = calculateDistance(
       RESTAURANT_LAT,
       RESTAURANT_LNG,
       userLat,
-      userLng
+      userLng,
     );
-
-
 
     // MAX DISTANCE
     if (distance > 8) {
@@ -71,8 +58,6 @@ router.post("/", authMiddleware, async (req, res) => {
         message: "Delivery unavailable",
       });
     }
-
-
 
     // ORDER ITEMS
     const orderDetails = items.map((item) => ({
@@ -83,11 +68,7 @@ router.post("/", authMiddleware, async (req, res) => {
       shopName: item.shopName,
     }));
 
-
-
     const user = await userModel.findById(userId);
-
-
 
     // SAVE ORDER
     const order = new orderModel({
@@ -106,6 +87,7 @@ router.post("/", authMiddleware, async (req, res) => {
       // IMPORTANT
       // frontend status will come here
       status: status || "Pending",
+      razorpayOrderId: razorpayOrderId,
 
       distance,
 
@@ -114,22 +96,16 @@ router.post("/", authMiddleware, async (req, res) => {
       phone: user?.phone || deliveryInfo.phone || "",
     });
 
-
-
     await order.save();
 
     console.log("ORDER SAVED");
-
-
 
     res.status(201).json({
       success: true,
       message: "Order placed successfully",
       order,
     });
-
   } catch (err) {
-
     console.log("ORDER ERROR:", err);
 
     res.status(500).json({
@@ -139,40 +115,28 @@ router.post("/", authMiddleware, async (req, res) => {
   }
 });
 
-
-
 // ADMIN GET ALL ORDERS
 router.get("/", async (req, res) => {
   try {
-
     const orders = await orderModel.find().sort({ createdAt: -1 });
 
     res.json(orders);
-
   } catch (err) {
-
     console.log(err);
 
     res.status(500).send("Failed to fetch orders");
   }
 });
 
-
-
 // USER GET ORDERS
 router.get("/my-orders", authMiddleware, async (req, res) => {
   try {
-
     const userId = req.userId;
 
-    const orders = await orderModel
-      .find({ userId })
-      .sort({ createdAt: -1 });
+    const orders = await orderModel.find({ userId }).sort({ createdAt: -1 });
 
     res.json(orders);
-
   } catch (err) {
-
     console.log(err);
 
     res.status(500).json({
@@ -182,14 +146,10 @@ router.get("/my-orders", authMiddleware, async (req, res) => {
   }
 });
 
-
-
 // NEW ROUTE
 // PAYMENT SUCCESS UPDATE
 router.patch("/payment-success/:id", async (req, res) => {
-
   try {
-
     const updatedOrder = await orderModel.findByIdAndUpdate(
       req.params.id,
 
@@ -198,28 +158,21 @@ router.patch("/payment-success/:id", async (req, res) => {
         status: "Pending",
       },
 
-      { new: true }
+      { new: true },
     );
 
-
-
     if (!updatedOrder) {
-
       return res.status(404).json({
         success: false,
         message: "Order not found",
       });
     }
 
-
-
     res.json({
       success: true,
       order: updatedOrder,
     });
-
   } catch (error) {
-
     console.log(error);
 
     res.status(500).json({
@@ -229,65 +182,47 @@ router.patch("/payment-success/:id", async (req, res) => {
   }
 });
 
-
-
 // UPDATE STATUS
 router.patch("/:id", async (req, res) => {
   try {
-
     const { status } = req.body;
 
     const updatedOrder = await orderModel.findByIdAndUpdate(
       req.params.id,
       { status },
-      { new: true }
+      { new: true },
     );
-
-
 
     if (!updatedOrder) {
       return res.status(404).send("Order not found");
     }
 
-
-
     const io = req.app.get("io");
 
     io.emit("orderStatusUpdated", updatedOrder);
-
-
 
     res.json({
       message: "Order updated",
       order: updatedOrder,
     });
-
   } catch (err) {
-
     console.log(err);
 
     res.status(500).send("Failed to update");
   }
 });
 
-
-
 // DELETE ORDER
 router.delete("/:id", async (req, res) => {
   try {
-
     await orderModel.findByIdAndDelete(req.params.id);
 
     res.status(200).send("Order removed");
-
   } catch (err) {
-
     console.log(err);
 
     res.status(500).send("Delete failed");
   }
 });
-
-
 
 export default router;
